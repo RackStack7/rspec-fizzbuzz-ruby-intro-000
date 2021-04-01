987888e55a67b3e7fb62be7054944f9f09aa1437
James Daaran <jamesdaaran@gmail.com>
2021-04-01 13:12:08 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,8 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+def fizzbuzz(value)
+  input = value.to_i
+  if value / 3 == 0
+  puts  "Fizz"
+elsif value / 5 == 0
+  puts "Buzz"
+
+end
