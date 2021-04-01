21394f82b43a45e9f848f3700234905a84c6a790
James Daaran <jamesdaaran@gmail.com>
2021-04-01 13:40:28 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,13 @@
 value = gets.strip
   input = value.to_i
 #def fizzbuzz(input)
-  if input % 3 == 0
-    puts "Fizz"
-elsif input % 5 == 0
-    puts "Buzz"
+fizz_3 = input % 3
+fizz_5 = input % 5
 
+if fizz_3 == fizz_5
+  puts "FizzBuzz"
+elsif fizz_3 == 0
+  puts "Fizz"
+elsif fizz_5 == 0
+  puts "Buzz"
 end
